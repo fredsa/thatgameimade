fa187b587babccb08ed848d7910951c122b281be
Fred Sauer <fredsa@google.com>
2014-12-29 20:52:04 -0800

    level 2

--- app/src/main/java/sauer/thatgameimade/LevelHolder.java
+++ app/src/main/java/sauer/thatgameimade/LevelHolder.java
@@ -18,19 +18,19 @@ public class LevelHolder {
     @SuppressWarnings("unused")
     private static final String TAG = LevelHolder.class.getSimpleName();
 
-    private static final String FILENAME = "level1.dat";
-
     private final int blockSize;
     private final Context context;
-    private String assetDirectory;
+    private final int level;
+    private String assetRoot;
     private String backgroundFilename;
     private ArrayList<BlockInfo> blockList;
     private int[][] levelBlocks;
     private Bitmap backgroundBitmap;
 
-    private LevelHolder(Context context, String assetDirectory, String backgroundFilename, Bitmap backgroundBitmap, int blockSize, ArrayList<BlockInfo> blockList, int[][] levelBlocks) {
+    private LevelHolder(Context context, int level, String assetRoot, String backgroundFilename, Bitmap backgroundBitmap, int blockSize, ArrayList<BlockInfo> blockList, int[][] levelBlocks) {
         this.context = context;
-        this.assetDirectory = assetDirectory;
+        this.level = level;
+        this.assetRoot = assetRoot;
         this.backgroundFilename = backgroundFilename;
         this.backgroundBitmap = backgroundBitmap;
         this.blockSize = blockSize;
@@ -38,46 +38,82 @@ public class LevelHolder {
         this.levelBlocks = levelBlocks;
     }
 
-    public static LevelHolder load(Context context) throws IOException {
-        int blockSize = context.getResources().getInteger(R.integer.block_size);
+    public static LevelHolder load(Context context, int level) throws IOException {
+        String filename = getFilename(level);
+        int blockSize;
+        String assetRoot;
+        String backgroundFilename;
+        ArrayList<BlockInfo> blockList;
+        int[][] levelBlocks;
+        switch (level) {
+            case 1:
+                blockSize = 70;
+                backgroundFilename = "Platformer Art Complete Pack_0/Mushroom expansion/Backgrounds/bg_castle.png";
+                assetRoot = "platformerArt_v4/png";
+                blockList = makeBlockList(context.getResources().getAssets(), assetRoot);
+                break;
+            case 2:
+                blockSize = 21;
+                backgroundFilename = "Platformer Art Complete Pack_0/Mushroom expansion/Backgrounds/bg_shroom.png";
+                assetRoot = "Platformer Art Deluxe (Pixel)/spritesheet.png";
+                blockList = makeBlockList(context.getResources().getAssets(), assetRoot, blockSize, 2, 1);
+                break;
+            default:
+                throw new RuntimeException("Don't know how to create level " + level);
+        }
+        Bitmap backgroundBitmap = getBitmap(context.getResources().getAssets(), backgroundFilename);
+
         try {
-            Log.w(TAG, "LOADING LEVEL FROM " + FILENAME);
-            FileInputStream fileInputStream = context.openFileInput(FILENAME);
+            Log.w(TAG, "LOADING LEVEL " + level + " FROM " + filename);
+            FileInputStream fileInputStream = context.openFileInput(filename);
             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-            String assetDirectory = (String) objectInputStream.readObject();
-            String backgroundFilename = (String) objectInputStream.readObject();
-            Bitmap backgroundBitmap = getBitmap(context.getResources().getAssets(), backgroundFilename);
-            int[][] levelBlocks = (int[][]) objectInputStream.readObject();
+            blockSize = objectInputStream.read();
+            assetRoot = (String) objectInputStream.readObject();
+            backgroundFilename = (String) objectInputStream.readObject();
+            levelBlocks = (int[][]) objectInputStream.readObject();
             objectInputStream.close();
             fileInputStream.close();
-            ArrayList<BlockInfo> blockList = makeBlockList(context.getResources().getAssets(), assetDirectory);
-            LevelHolder levelHolder = new LevelHolder(context, assetDirectory, backgroundFilename, backgroundBitmap, blockSize, blockList, levelBlocks);
-            Log.w(TAG, "LEVEL LOADED FROM " + FILENAME);
-            return levelHolder;
+            Log.w(TAG, "LEVEL " + level + " LOADED FROM " + filename);
         } catch (Exception e) {
-            Log.w("Unable to deserialize file '" + FILENAME + "'", e);
+            Log.w("Deserialize failure '" + filename + "'", e);
+            int levelBlocksX = (int) Math.floor(backgroundBitmap.getWidth() / blockSize);
+            int levelBlocksY = (int) Math.floor(backgroundBitmap.getHeight() / blockSize);
+            levelBlocks = makeLevelBlocks(blockList, levelBlocksX, levelBlocksY);
         }
-        String assetDirectory = "platformerArt_v4/png";
-        String backgroundFilename = "Platformer Art Complete Pack_0/Mushroom expansion/Backgrounds/bg_castle.png";
-        Bitmap backgroundBitmap = getBitmap(context.getResources().getAssets(), backgroundFilename);
-        int levelBlocksX = (int) Math.floor(backgroundBitmap.getWidth() / blockSize);
-        int levelBlocksY = (int) Math.floor(backgroundBitmap.getHeight() / blockSize);
-        ArrayList<BlockInfo> blockList = makeBlockList(context.getResources().getAssets(), assetDirectory);
-        int[][] levelBlocks = makeLevelBlocks(blockList, levelBlocksX, levelBlocksY);
-        LevelHolder levelHolder = new LevelHolder(context, assetDirectory, backgroundFilename, backgroundBitmap, blockSize, blockList, levelBlocks);
-        Log.w(TAG, "NEW LEVEL DATA GENERATED");
+        LevelHolder levelHolder = new LevelHolder(context, level, assetRoot, backgroundFilename, backgroundBitmap, blockSize, blockList, levelBlocks);
+        Log.w(TAG, "NEW LEVEL " + level + " DATA GENERATED");
         return levelHolder;
     }
 
-    private static Bitmap getBitmap(AssetManager assetManager, String assetFilename) throws IOException {
-            return BitmapFactory.decodeStream(assetManager.open(assetFilename));
+    private static String getFilename(int level) {
+        return "level" + level + ".dat";
     }
 
-    private static ArrayList<BlockInfo> makeBlockList(AssetManager assetManager, String assetDirectory) throws IOException {
+    private static ArrayList<BlockInfo> makeBlockList(AssetManager assetManager, String path, int blockSize, int margin, int trim) throws IOException {
         ArrayList<BlockInfo> blockList = new ArrayList<>();
-        String[] items = assetManager.list(assetDirectory);
+        Bitmap bitmap = getBitmap(assetManager, path);
+        int stride = blockSize + margin;
+        int blocksX = (bitmap.getWidth() - 2 * trim) / stride;
+        int blocksY = (bitmap.getHeight() - 2 * trim) / stride;
+        for (int i = 0; i < blocksX; i++) {
+            for (int j = 0; j < blocksY; j++) {
+                // TODO avoid wasting memory on bitmap copies
+                Bitmap b = Bitmap.createBitmap(bitmap, trim + margin + i * stride, trim + margin + j * stride, blockSize, blockSize);
+                blockList.add(new BlockInfo(path, b));
+            }
+        }
+        return blockList;
+    }
+
+    private static Bitmap getBitmap(AssetManager assetManager, String assetFilename) throws IOException {
+        return BitmapFactory.decodeStream(assetManager.open(assetFilename));
+    }
+
+    private static ArrayList<BlockInfo> makeBlockList(AssetManager assetManager, String assetRoot) throws IOException {
+        ArrayList<BlockInfo> blockList = new ArrayList<>();
+        String[] items = assetManager.list(assetRoot);
         for (String item : items) {
-            String path = assetDirectory + "/" + item;
+            String path = assetRoot + "/" + item;
             try {
                 Bitmap bitmap = getBitmap(assetManager, path);
                 bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
@@ -125,18 +161,20 @@ public class LevelHolder {
     }
 
     public void save() {
-        Log.w(TAG, "SAVING LEVEL TO " + FILENAME);
+        String filename = getFilename(level);
+        Log.w(TAG, "SAVING LEVEL TO " + filename);
         try {
-            FileOutputStream fileOutputStream = context.openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            FileOutputStream fileOutputStream = context.openFileOutput(filename, Context.MODE_PRIVATE);
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-            objectOutputStream.writeObject(assetDirectory);
+            objectOutputStream.write(blockSize);
+            objectOutputStream.writeObject(assetRoot);
             objectOutputStream.writeObject(backgroundFilename);
             objectOutputStream.writeObject(levelBlocks);
             objectOutputStream.close();
             fileOutputStream.close();
         } catch (IOException e) {
-            throw new RuntimeException("Unable to open file '" + FILENAME + "'", e);
+            throw new RuntimeException("Unable to open file '" + filename + "'", e);
         }
-        Log.w(TAG, "LEVEL SAVED TO " + FILENAME);
+        Log.w(TAG, "LEVEL SAVED TO " + filename);
     }
 }
--- app/src/main/java/sauer/thatgameimade/MyApplication.java
+++ app/src/main/java/sauer/thatgameimade/MyApplication.java
@@ -11,19 +11,21 @@ public class MyApplication extends Application {
     private static final String TAG = MyApplication.class.getSimpleName();
 
     private LevelHolder level1;
+    private LevelHolder level2;
 
     @Override
     public void onCreate() {
         super.onCreate();
         Log.w(TAG, "=========================== " + new Date());
         try {
-            level1 = LevelHolder.load(getApplicationContext());
+            level1 = LevelHolder.load(getApplicationContext(), 1);
+            level2 = LevelHolder.load(getApplicationContext(), 2);
         } catch (IOException e) {
             throw new RuntimeException("Unable to load level data", e);
         }
     }
 
     public LevelHolder getLevelHolder() {
-        return level1;
+        return level2;
     }
 }
